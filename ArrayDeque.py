b31411bea2236e4d958c12bd4ab907b9a8c00e31
MariaFernandaOrtega <229757@students.hertie-school.org>
2023-03-19 12:11:25 +0100

    answers

--- ArrayDeque.py
+++ ArrayDeque.py
@@ -2,15 +2,44 @@
 """
 Created on Sun Mar  5 18:34:01 2023
 
-@author: Hannah
+@author: Maria Fernanda Ortega
 """
 
-class Empty(Exception): pass
+class Deque:
+    def __init__(self):
+        self.items = []
 
+    def is_empty(self):
+        return len(self.items) == 0
 
-class ArrayDequeMaxlen(): pass
-        
-                
-        
+    def add_first(self, item):
+        self.items.insert(0, item)
+
+    def add_last(self, item):
+        self.items.append(item)
+
+    def delete_first(self):
+        if self.is_empty():
+            return None
+        return self.items.pop(0)
+
+    def delete_last(self):
+        if self.is_empty():
+            return None
+        return self.items.pop()
+
+    def first(self):
+        if self.is_empty():
+            return None
+        return self.items[0]
+
+    def last(self):
+        if self.is_empty():
+            return None
+        return self.items[-1]
+
+    def __len__(self):
+        return len(self.items)
+
         
    
--- main.py
+++ main.py
@@ -2,36 +2,58 @@
 """
 Created on Sun Mar  5 18:34:20 2023
 
-@author: Hannah
+@author: Maria Fernanda Ortega
 """
 
-from ArrayDeque import ArrayDequeMaxlen
+from ArrayDeque import Deque
 
-     
-        
-AQM = ArrayDequeMaxlen(20)
+d = Deque()
+print(d.items)
 
+for i in range(10):
+    d.add_last(i)
+    print(d.items)
 
-print('Adding last')
-for i in range(100):
-    AQM.append(i)
-    print (i, AQM._data)
-    
-print('\nDelete 80', AQM.remove(80), AQM._data, AQM._front)
-    
+for i in range(10):
+    d.add_first(i)
+    print(d.items)
 
+for i in range(10):
+    d.delete_last()
+    print(d.items)
 
-    
-print ('\nAdding first')
-for i in range(20, 10, -1):
-    AQM.appendleft(i)
-    print (i, AQM._data)
-    
-    
-print(AQM._front)
-    
+for i in range(10):
+    d.delete_first()
+    print(d.items)
 
-    
-print('\nPerforming the removals')
-while not AQM.is_empty():
-    print ('Remove first', AQM[0], AQM.popleft(), 'Remove last', AQM[-1],  AQM.pop())
+for i in range(20):
+    d.add_last(i)
+    print(d.items)
+
+for i in range(20):
+    d.add_first(i)
+    print(d.items)
+
+for i in range(20):
+    d.delete_last()
+    print(d.items)
+
+for i in range(20):
+    d.delete_first()
+    print(d.items)
+
+d.add_first(20)
+print(d.items)
+
+d.add_last(21)
+print(d.items)
+
+d.add_first(22)
+print(d.items)
+
+d.add_last(23)
+print(d.items)
+
+d.add_first(24)
+print(d.items)
+
